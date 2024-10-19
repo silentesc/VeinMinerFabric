03a26e78c69e6d53f082de5da554da2501e66676
silentesc <scope-frisk-copy@duck.com>
2024-10-19 21:04:19 +0200

    Implement logic for ores and logs

--- src/main/java/de/silentesc/veinminer/events/PlayerBlockBreakEvent.java
+++ src/main/java/de/silentesc/veinminer/events/PlayerBlockBreakEvent.java
@@ -1,53 +1,47 @@
 package de.silentesc.veinminer.events;
 
-import de.silentesc.veinminer.VeinMiner;
-import de.silentesc.veinminer.data.TreeCapitatorBlocks;
+import de.silentesc.veinminer.classes.VeinMiner;
+import de.silentesc.veinminer.classes.VeinMinerBlock;
 import de.silentesc.veinminer.data.VeinMinerBlocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.AxeItem;
 import net.minecraft.item.ItemStack;
-import net.minecraft.server.world.ServerWorld;
+import net.minecraft.item.PickaxeItem;
+import net.minecraft.item.ToolItem;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 import java.util.List;
 
 public class PlayerBlockBreakEvent {
+    /*
+     * Returns false to cancel event
+     */
     public static boolean onPlayerBlockBreakEventBefore(World world, PlayerEntity player, BlockPos pos, BlockState state, BlockEntity blockEntity) {
-        // Return if code is running on client
-        if (world.isClient()) {
-            return true;
-        }
-
         Block block = state.getBlock();
+        ItemStack mainHandItemStack = player.getMainHandStack();
 
-        // Tree Capitator
-        if (TreeCapitatorBlocks.getLogBlocks().contains(block)) {
-            return false;
-        }
+        // Checks
+        if (world.isClient())
+            return true;
+        if (player.isCreative())
+            return true;
+        if (!(mainHandItemStack.getItem() instanceof ToolItem mainHandToolItem))
+            return true;
+        if (!(mainHandToolItem instanceof AxeItem && VeinMinerBlocks.getLogBlocks().contains(block)) && !(mainHandToolItem instanceof PickaxeItem && VeinMinerBlocks.getOreBlocks().contains(block)))
+            return true;
+        if (!mainHandToolItem.isCorrectForDrops(mainHandItemStack, state))
+            return true;
 
-        // Vein Miner
-        if (VeinMinerBlocks.getOreBlocks().contains(block)) {
-            List<ItemStack> droppedItems =  Block.getDroppedStacks(state, (ServerWorld) world, pos, blockEntity, player, player.getMainHandStack());
+        VeinMinerBlock originalBlock = new VeinMinerBlock(pos, state, blockEntity);
+        List<Block> blocksToMine = VeinMinerBlocks.getLogBlocks().contains(state.getBlock()) ? VeinMinerBlocks.getLogBlocks() : VeinMinerBlocks.getOreBlocks();
+        VeinMiner veinMiner = new VeinMiner(blocksToMine, originalBlock, world, player);
 
-            
+        veinMiner.startVeinMiner();
 
-            for (ItemStack itemStack : droppedItems) {
-                VeinMiner.LOGGER.info("Dropped Item: {} x{}", itemStack.getItem().getName().getString(), itemStack.getCount());
-                // player.giveItemStack(itemStack);
-            }
-            // world.removeBlockEntity(pos);
-
-            // TODO
-            // Check if tool is ok
-            // Get item & xp drops for tool (Fortune? Silk Touch?)
-            // Break block
-            // Search blocks around and break if fitting
-            return false;
-        }
-
-        return true;
+        return false;
     }
 }
